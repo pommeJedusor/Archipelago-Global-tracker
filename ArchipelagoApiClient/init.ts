57d589a6af68df20145d05bfccc683267b51b198
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2026-03-19 00:33:28 +0100

    can change the websocket address by putting ?address=ws://localhost:8765 in the url

--- ArchipelagoApiClient/init.ts
+++ ArchipelagoApiClient/init.ts
@@ -1,8 +1,10 @@
 import { ArchipelagoApiClient } from "./ArchipelagoApiClient";
 
-export function init(client: ArchipelagoApiClient, address: string = "ws://localhost:8765"){
-  client.socket = new WebSocket(address)
+export function init(client: ArchipelagoApiClient){
+  const address_parameter = location.href.split("?")[1]?.split("&").map((parameter)=>parameter.split("=")).filter((parameter)=>parameter[0]=="address")[0];
+  const address = address_parameter == undefined ? "ws://localhost:8765" : address_parameter[1];
 
+  client.socket = new WebSocket(address)
   client.socket.addEventListener("message", (event) => {
     const players = JSON.parse(event.data);
     client.players = players
