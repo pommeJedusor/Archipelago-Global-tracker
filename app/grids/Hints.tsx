70311a58bce109cb32fffdcd5a10c6bab2201d6a
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2026-03-20 22:12:43 +0100

    fixed max-height and made the headers sticky

--- app/grids/Hints.tsx
+++ app/grids/Hints.tsx
@@ -1,11 +1,11 @@
-import { ArchipelagoApiClient, Hint, Player } from "@/ArchipelagoApiClient/ArchipelagoApiClient";
+import { ArchipelagoApiClient, Hint } from "@/ArchipelagoApiClient/ArchipelagoApiClient";
 
 const ROW_SIZE = 24; // px
 const GAP_SIZE = 2; // px
 
 function GridHeadersRow() {
   return (
-    <>
+    <div className="grid grid-cols-100 gap-[2px] sticky top-0">
       <div className="bg-light-brown h-[24px] font-bold flex flex-col justify-center px-2 col-span-9"><p>Finder</p></div>
       <div className="bg-light-brown h-[24px] font-bold flex flex-col justify-center px-2 col-span-9"><p>Receiver</p></div>
       <div className="bg-light-brown h-[24px] font-bold flex flex-col justify-center px-2 col-span-16"><p>Item</p></div>
@@ -13,7 +13,7 @@ function GridHeadersRow() {
       <div className="bg-light-brown h-[24px] font-bold flex flex-col justify-center px-2 col-span-11"><p>Game</p></div>
       <div className="bg-light-brown h-[24px] font-bold flex flex-col justify-center px-2 col-span-24"><p>Entrance</p></div>
       <div className="bg-light-brown h-[24px] font-bold flex flex-col justify-center text-center col-span-4"><p>Found</p></div>
-    </>
+    </div>
   )
 }
 
@@ -30,7 +30,7 @@ function HintRow({hint, client}: {hint: Hint, client: ArchipelagoApiClient}) {
   const location = Object.entries(sender_datapackage.location_name_to_id).filter((location)=>location[1] == hint.location)[0][0]
 
   return (
-    <>
+    <div className="grid grid-cols-100 gap-[2px]">
       <div className="bg-light-green h-[24px] px-2 col-span-9">{client.players[hint.finding_player - 1].name}</div>
       <div className="bg-light-green h-[24px] px-2 col-span-9">{receiving_player}</div>
       <div className="bg-light-green h-[24px] px-2 col-span-16">{item}</div>
@@ -38,12 +38,16 @@ function HintRow({hint, client}: {hint: Hint, client: ArchipelagoApiClient}) {
       <div className="bg-light-green h-[24px] px-2 col-span-11">{client.players[hint.finding_player - 1].game}</div>
       <div className="bg-light-green h-[24px] px-2 col-span-24">{hint.entrance || "Vanilla"}</div>
       <div className="bg-light-green h-[24px] text-center col-span-4">{hint.found ? "✔" : ""}</div>
-    </>
+    </div>
   )
 }
 
 export default function Hints({client}: {client: ArchipelagoApiClient}) {
   const hints = client.players?.map((player)=>player.hints || []).flat(1) || []
+  const numberHint = hints.length || 0;
+  const numberRow = numberHint + 1;
+  const gapNumber = numberHint;
+  const div_style = {maxHeight: `min(${numberRow * ROW_SIZE + gapNumber * GAP_SIZE}px, 45vh)`};
 
   let hint_rows = []
   for (let i=0;i<hints.length;i++){
@@ -52,7 +56,7 @@ export default function Hints({client}: {client: ArchipelagoApiClient}) {
   }
 
   return (
-    <div className={"bg-light-brown h-min max-h-[45vh] resize-y overflow-auto overflow-x-hidden grid grid-cols-100 gap-[2px] mx-2 mb-4"}>
+    <div style={div_style} className={"bg-light-brown h-min resize-y overflow-auto overflow-x-hidden grid gap-[2px] mx-2 mb-4"}>
       <GridHeadersRow />
       {hint_rows}
     </div>
--- app/grids/Players.tsx
+++ app/grids/Players.tsx
@@ -10,7 +10,7 @@ function number_to_two_digit_number(x: number){
 
 function GridHeadersRow() {
   return (
-    <>
+    <div className="grid grid-cols-100 gap-[2px] sticky top-0">
       <div className="bg-light-brown h-[48px] font-bold flex flex-col justify-center px-2 col-span-5"><p>#</p></div>
       <div className="bg-light-brown h-[48px] font-bold flex flex-col justify-center px-2 col-span-20"><p>Name</p></div>
       <div className="bg-light-brown h-[48px] font-bold flex flex-col justify-center px-2 col-span-25"><p>Game</p></div>
@@ -18,7 +18,7 @@ function GridHeadersRow() {
       <div className="bg-light-brown h-[48px] font-bold flex flex-col justify-center text-center col-span-12"><p>Checks</p></div>
       <div className="bg-light-brown h-[48px] font-bold flex flex-col justify-center text-center col-span-8"><p>%</p></div>
       <div className="bg-light-brown h-[48px] font-bold flex flex-col justify-center text-center col-span-12"><p>Last Activity</p></div>
-    </>
+    </div>
   )
 }
 
@@ -28,7 +28,7 @@ function PlayerRow({player}: {player: Player}) {
   const percentage_check = ((player.checks?.length || 0) / player.location_number * 100).toFixed(2)
 
   return (
-    <>
+    <div className="grid grid-cols-100 gap-[2px]">
       <div className="bg-light-green h-[24px] font-bold px-2 col-span-5"><a className="text-red-700 hover:underline" href="">{player.slot}</a></div>
       <div className="bg-light-green h-[24px] px-2 col-span-20">{player.name}</div>
       <div className="bg-light-green h-[24px] px-2 col-span-25">{player.game || "unknown"}</div>
@@ -36,7 +36,7 @@ function PlayerRow({player}: {player: Player}) {
       <div className="bg-light-green h-[24px] text-center col-span-12">{player.checks?.length || 0}/{player.location_number}</div>
       <div className="bg-light-green h-[24px] text-center col-span-8">{percentage_check}</div>
       <div className="bg-light-green h-[24px] text-center col-span-12">{last_activity}</div>
-    </>
+    </div>
   )
 }
 
@@ -50,14 +50,14 @@ function TotalRow({client}: {client: ArchipelagoApiClient}){
   const last_activity = minutes_elapseds == undefined ? "None" : `${Math.floor(Math.min(...minutes_elapseds) / 60)}:${number_to_two_digit_number(Math.min(...minutes_elapseds) % 60)}`;
 
   return (
-    <>
+    <div className="grid grid-cols-100 gap-[2px]">
       <p className="bg-light-green h-[24px] font-bold px-2 col-span-25 text-right">Total</p>
       <p className="bg-light-green h-[24px] font-bold px-2 col-span-25">All Games</p>
       <p className="bg-light-green h-[24px] font-bold px-2 col-span-18">{client.players == undefined ? "N/A" : `${number_of_goal_completed}/${number_of_players} Complete`}</p>
       <p className="bg-light-green h-[24px] font-bold text-center col-span-12">{number_of_checks}/{number_of_locations}</p>
       <p className="bg-light-green h-[24px] font-bold text-center col-span-8">{percentage_check}</p>
       <p className="bg-light-green h-[24px] font-bold text-center col-span-12">{last_activity}</p>
-    </>
+    </div>
   )
 }
 
@@ -67,8 +67,13 @@ export default function Players({client}: {client: ArchipelagoApiClient}) {
     players.push(<PlayerRow player={player} key={(player.team, player.slot)} />)
   }
 
+  const numberPlayer = client.players?.length || 0;
+  const numberRow = numberPlayer + 3; // count the headers as two
+  const gapNumber = numberPlayer + 1;
+  const div_style = {maxHeight: `min(${numberRow * ROW_SIZE + gapNumber * GAP_SIZE}px, 45vh)`};
+
   return (
-    <div className={"bg-light-brown h-min max-h-[45vh] resize-y overflow-auto overflow-x-hidden grid grid-cols-100 gap-[2px] mx-2 mb-4"}>
+    <div style={div_style} className={"bg-light-brown h-min resize-y overflow-auto overflow-x-hidden grid gap-[2px] mx-2 mb-4"}>
       <GridHeadersRow />
       {players}
       <TotalRow client={client} />
